9c8653d035f5f3062a11140ad31a4617872aa4ba
Colm Sheehan <colms152@gmail.com>
2020-04-18 18:58:33 +0100

    fix

--- server.js
+++ server.js
@@ -28,10 +28,24 @@ app.get("/api/timestamp/", (req, res) => {
   res.json({ unix: Date.now(), utc: Date() });
 });
 
-app.get("/api/timestamp/:date", (req, res) => {
-  var dateString = req.params.date;
+app.get("/api/timestamp/:date_string", (req, res) => {
+  var dateString = req.params.date_string;
 
-  res.json({ unix: dateString, utc: Date() });
+  //A 4 digit number is a valid ISO-8601 for the beginning of that year
+  //5 digits or more must be a unix time, until we reach a year 10,000 problem
+  if (/\d{5,}/.test(dateString)) {
+    dateInt = parseInt(dateString);
+    //Date regards numbers as unix timestamps, strings are processed differently
+    res.json({ unix: dateString, utc: new Date(dateInt).toUTCString() });
+  }
+
+  let dateObject = new Date(dateString);
+
+  if (dateObject.toString() === "Invalid Date") {
+    res.json({ error: "Invaid Date" });
+  } else {
+    res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
+  }
 });
 
 
